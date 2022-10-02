73e0c84dbe868a90a2557a63cf0c2e56df9225b1
MiguelDotL <miguel.d.lozano@gmail.com>
2022-10-01 23:14:19 -0400

    update env var implementation

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-if (process.env.NODE_ENV !== "production") {
-    var { REACT_APP_UNSPLASH_ACCESS_KEY } = process.env;
-}
-
+const { REACT_APP_UNSPLASH_ACCESS_KEY } = process.env;
 const ACCESS_KEY = REACT_APP_UNSPLASH_ACCESS_KEY;
 
 // create new instance of axios
